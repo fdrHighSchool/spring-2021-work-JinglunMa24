4c0f9915f08a27eb6d533e2513fb837fc74a6c91
JinglunMa24 <2461447099ma@gmail.com>
2021-01-20 09:16:20 -0500

    set basis

--- 00-SuperArray/Driver.java
+++ 00-SuperArray/Driver.java
@@ -1,5 +1,9 @@
 public class Driver {
   public static void main(String[] args) {
-    
+    SuperArray arr1 = new SuperArray(5);
+    arr1.add(90);
+    System.out.println(arr1);
+
   } //end main
+
 } //end class
--- 00-SuperArray/SuperArray.java
+++ 00-SuperArray/SuperArray.java
@@ -1,3 +1,45 @@
+import java.util.*;
+
 public class SuperArray {
-  
+  private int[] array;
+  private int numValues;
+
+  public SuperArray(int size) {
+    this.array = new int[size];
+    this.numValues = 0;
+  } //end structor
+
+  public SuperArray() {
+    this.array = new int[10];
+  } //end overloaded
+
+  public void add(int val) {
+    this.array[this.numValues] = val;
+    this.numValues++;
+  } //end add()
+
+  public void add(int index, int val) {
+
+  } //end second add()
+
+  public void grow(int n) {
+
+  } //end grow()
+
+  public void set(int i, int val) {
+
+  } //end set()
+
+  public boolean isEmpty() {
+    return true;
+  } //end isEmpty()
+
+  public void remove(int index) {
+
+  } //end remove()
+
+  public String toString() {
+    return Arrays.toString(this.array);
+  } //end toString()
+
 } //end class
