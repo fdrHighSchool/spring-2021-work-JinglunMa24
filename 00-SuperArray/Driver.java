3fb5d3507bcbd80603c94feabcca74d0d781c822
JinglunMa24 <2461447099ma@gmail.com>
2021-01-21 09:17:08 -0500

    added assignment

--- 00-SuperArray/Driver.java
+++ 00-SuperArray/Driver.java
@@ -2,6 +2,14 @@ public class Driver {
   public static void main(String[] args) {
     SuperArray arr1 = new SuperArray(5);
     arr1.add(90);
+    arr1.add(100);
+    arr1.add(95);
+    arr1.add(95);
+    arr1.add(95);
+    System.out.println(arr1);
+    System.out.println(arr1.isEmpty());
+    arr1.grow(2);
+    arr1.add(95);
     System.out.println(arr1);
 
   } //end main
