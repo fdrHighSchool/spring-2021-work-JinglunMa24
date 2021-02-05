603995aac47fac14fb250f83c87e623d2c4e5a25
JinglunMa24 <2461447099ma@gmail.com>
2021-02-05 09:01:20 -0500

    added assignment

--- 00-SuperArray/Driver2.java
+++ 00-SuperArray/Driver2.java
@@ -1,6 +1,12 @@
 public class Driver2 {
   public static void main(String[] args) {
     GradeBook book1 = new GradeBook();
+    book1.addExamGrade(90);
+    book1.addExamGrade(100);
+    book1.addExamGrade(95);
+    book1.addHWGrade(90);
+    System.out.println(book1);
+    book1.getExamAverage();
 
   } //end main
 
