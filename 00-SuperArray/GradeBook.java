80ff0e5b821982c804d785d5b4bf32e969482a07
JinglunMa24 <2461447099ma@gmail.com>
2021-02-09 09:20:23 -0500

    added assignment

--- 00-SuperArray/GradeBook.java
+++ 00-SuperArray/GradeBook.java
@@ -1,6 +1,8 @@
 public class GradeBook {
   SuperArray exam;
   SuperArray homework;
+  private double examAverage;
+  private double homeworkAverage;
 
   public GradeBook() {
     this.exam = new SuperArray("exam");
@@ -15,13 +17,43 @@ public class GradeBook {
     this.homework.add(val);
   } //end addHWGrade()
 
-  public double getExamAverage() {
+  public double getAverage(SuperArray arr) {
     double sum = 0;
-    for (int i = 0; i < this.exam.getSize(); i++) {
-      sum += this.exam.getValues(i);
+    for (int i = 0; i < arr.getSize(); i++) {
+      sum += arr.getValues(i);
     } //end for loop
-    return sum / this.exam.getSize();
-  } //end getExamAverage()
+    return sum / arr.getSize();
+  } //end getAverage()
+
+  public void replaceExamLowestValue(int val) {
+    int index = 0;
+    int lowest = this.exam.getValues(0);
+    for (int i = 0; i < this.exam.getSize(); i++) {
+      if (this.exam.getValues(i) < lowest) {
+        lowest = this.exam.getValues(i);
+        index = i;
+      } //end
+    } //end for loop
+    this.exam.add(index, val);
+  } //end replaceLowest()
+
+  public void replaceHWLowestValue(int val) {
+    int index = 0;
+    int lowest = this.homework.getValues(0);
+    for (int i = 0; i < this.homework.getSize(); i++) {
+      if (this.homework.getValues(i) < lowest) {
+        lowest = this.homework.getValues(i);
+        index = i;
+      } //end
+    } //end for loop
+    this.homework.add(index, val);
+  } //end replaceLowest()
+
+  public void displayAverages() {
+    System.out.println("exam average: " + getAverage(this.exam));
+    System.out.println("homework average: " + getAverage(this.homework));
+    System.out.println("Total average: " + (getAverage(this.exam) + getAverage(this.homework)) / 2);
+  } //end displayAverages()
 
   public String toString() {
     String output = "";
