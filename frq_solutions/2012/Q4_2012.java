5a9fccbf315994de545f0b1d89acc47128f80798
JinglunMa24 <2461447099ma@gmail.com>
2021-03-09 09:06:05 -0500

    added assignment

--- frq_solutions/2012/Q4_2012.java
+++ frq_solutions/2012/Q4_2012.java
@@ -12,11 +12,9 @@ public class Q4_2012 {
   } //end countWhitePixels()
 
   public void processImage() {
-    for (int row = 0; row < pixelValues.length; row++) {
-      for (int col = 0; col < pixelValues[row].length; col++) {
-        if (row - 2 >= 0 && col - 2 >= 0) {
-          pixelValues[row][col] -= pixelValues[row - 2][col - 2];
-        } //end for loop
+    for (int row = 0; row < pixelValues.length - 2; row++) {
+      for (int col = 0; col < pixelValues[row].length - 2; col++) {
+        pixelValues[row][col] -= pixelValues[row + 2][col + 2];
       } //end for col loop
     } //end for row loop
   } //end processImage()
