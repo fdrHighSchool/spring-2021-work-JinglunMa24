e81b2b4a1883a5df4c15f6bc86d5a5b824a2b96f
JinglunMa24 <2461447099ma@gmail.com>
2021-03-09 09:15:13 -0500

    added assignment

--- frq_solutions/2012/Q4_2012.java
+++ frq_solutions/2012/Q4_2012.java
@@ -16,9 +16,11 @@ public class Q4_2012 {
 
   // Part B
   public void processImage() {
-    for (int row = 0; row < pixelValues.length - 2; row++) {
-      for (int col = 0; col < pixelValues[row].length - 2; col++) {
-        pixelValues[row][col] -= pixelValues[row + 2][col + 2];
+    for (int row = 0; row < pixelValues.length; row++) {
+      for (int col = 0; col < pixelValues[row].length; col++) {
+        if (row + 2 < pixelValues.length && col + 2 < pixelValues[row].length) {
+          pixelValues[row][col] -= pixelValues[row + 2][col + 2];
+        } //end if
         if (pixelValues[row][col] < BLACK) {
           pixelValues[row][col] == BLACK;
         } //end if
