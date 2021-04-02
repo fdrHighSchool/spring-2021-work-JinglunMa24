eacc65e58619e2b558e6c6b7e74e46c89176722c
Jinglun Ma <2461447099ma@gmail.com>
2021-04-01 21:08:18 -0400

    added assignment

---
+++ frq_solutions/2016/Q2_2016.java
@@ -0,0 +1,21 @@
+public class Q2_2016 {
+
+  // Part A
+  public LogMessage(String message) {
+    this.machineId = message.substring(0, message.indexOf(":"));
+    this.description = message.substring(message.indexOf(":") + 1);
+  } //end LogMessage()
+
+
+  // Part B
+  public boolean containsWord(String keyword) {
+    int index = this.description.indexOf(keyword);
+    if (index != -1) {
+      boolean check1 = (index == 0) || (this.description.charAt(index - 1).equals(" "));
+      boolean check2 = (index == this.description.length() - keyword.length()) || (description.charAt(index + keyword.length() + 1).equals(" "));
+      return check1 && check2;
+    } //end if
+    return false;
+  } //end containsWord()
+
+} //end Q2_2016
