e76a65daac3f68f9fa32ee119c6537c5ac9c07e1
Jinglun Ma <2461447099ma@gmail.com>
2021-04-05 09:27:03 -0400

    added assignment

--- frq_solutions/2016/Q1_2016.java
+++ frq_solutions/2016/Q1_2016.java
@@ -2,9 +2,9 @@ public class Q1_2016 {
 
   // Part A
   public class RandomStringChooser {
-    private ArrayList<String> arr;
-    public RandomStringChooser(String[] a) {
-      this.arr = new ArrayList<String>();
+    private ArrayList<String> arr;  // 1 point
+    public RandomStringChooser(String[] a) {  // 1 point
+      this.arr = new ArrayList<String>(); // 1 point
       for (String word : a) {
         this.arr.add(word);
       } //end for each loop
@@ -12,17 +12,19 @@ public class Q1_2016 {
 
     public String getNext() {
       if (this.arr.size() == 0) {
-        return "NONE";
+        return "NONE";  // 1 point
       } //end if
-      int index = (int)(Math.random() * this.arr.size());
-      return this.arr.remove(index);
+      int index = (int)(Math.random() * this.arr.size());  // 1 point
+      return this.arr.remove(index);  // 2 points
     } //end getNext()
   } //end class
 
 
   // Part B
   public RandomLetterChooser(String str) {
-    super(getSingleLetters(str));
+    super(getSingleLetters(str));   // 2 points
   } //end RandomLetterChooser
+
+  // Total points earned: 7 points
 
 } //end Q1_2016
